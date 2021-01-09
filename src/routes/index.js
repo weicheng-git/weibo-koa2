71b683d5a0d4c45800c04c8b2124acfa2204fd5c
chenweicheng <chenweicheng@wuzhenpay.com>
2021-01-09 14:39:17 +0800

    feat: 路由演示

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,13 +6,26 @@ router.get('/', async (ctx, next) => {
   })
 })
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
-
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
+  }
+})
+
+router.get('/profile/:username', async (ctx, next) => {
+  const { username } = ctx.params
+  ctx.body = {
+    title: 'This is profile page',
+    username
+  }
+})
+
+router.get('/load-more/:username/:page', async (ctx, next) => {
+  const { username, page } = ctx.params
+  ctx.body = {
+    title: 'This is loadMore API',
+    username,
+    page
   }
 })
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -10,4 +10,14 @@ router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
 
+router.post('/login', async (ctx, next) => {
+  /* ctx.request.body 获取请求参数 */
+  const { username, password } = ctx.request.body
+
+  ctx.body = {
+    username,
+    password
+  }
+})
+
 module.exports = router
